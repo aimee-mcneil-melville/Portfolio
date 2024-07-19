50e56f02dcd72a0a230a0bbb53bf08b551419df1
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-07-19 23:53:45 +1200

    setting up database

---
+++ db/config.ts
@@ -0,0 +1,24 @@
+import { defineDb, defineTable, column } from 'astro:db'
+
+const Portfolio = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    title: column.text(),
+    subtitle: column.text(),
+    image: column.text(),
+    goal: column.text(),
+    text: column.text(),
+    tech: column.text(),
+  },
+})
+
+const Tech = defineTable({
+  columns: {
+    portfolioId: column.number({ references: () => Portfolio.columns.id }),
+    name: column.text(),
+  },
+})
+
+export default defineDb({
+  tables: { Portfolio, Tech },
+})
---
+++ db/seed.ts
@@ -0,0 +1,34 @@
+import { db, Portfolio, Tech } from 'astro:db'
+
+export default async function () {
+  await db.insert(Portfolio).values([
+    {
+      title: 'Trip Hive',
+      subtitle:
+        "My team's final group project at Dev Academy. We continue to add new features.",
+      image: '/image1.jpeg',
+      goal: 'Create an application that can be used by members of the group and eventually marketed to the public.',
+      text: 'this is a blurb about the project',
+      tech: 'react',
+    },
+    {
+      title: 'Hair Salon Website',
+      subtitle: 'A team project to build a new website for a hair salon.',
+      image: '/image2.jpg',
+      goal: 'A project to learn new technologies and simulate a real-world scenario.',
+      text: 'this is another project blurb',
+      tech: 'astro',
+    },
+  ])
+
+  await db.insert(Tech).values([
+    { portfolioId: 1, name: 'React' },
+    { portfolioId: 1, name: 'Typescript' },
+    { portfolioId: 1, name: 'Knex' },
+    { portfolioId: 1, name: 'SqLite3' },
+    { portfolioId: 1, name: 'Bulma' },
+    { portfolioId: 2, name: 'Astro' },
+    { portfolioId: 2, name: 'Typescript' },
+    { portfolioId: 2, name: 'Tailwind' },
+  ])
+}
